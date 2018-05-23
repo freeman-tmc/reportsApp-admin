ef0cfcf74fa0ecfde2389c9707b7252b3e3cb4bb
freeman-tmc <freeman.tomic84@gmail.com>
2018-05-23 14:18:34 +0200

    validation finished

--- src/app/pages/submitreportpage/ReportForm.js
+++ src/app/pages/submitreportpage/ReportForm.js
@@ -6,35 +6,50 @@ class ReportForm extends Component {
         super(props);
         this.state = {
             dateInput: '',
-            phaseInput: 'technical',
+            phaseInput: '',
             statusInput: '',
             noteInput: '',
+            disableSubmit: false,
+            dateError: false,
         }
     }
 
     handleChange = (event) => {
 
         switch (event.target.name) {
+
             case 'date':
+                let enteredDate = new Date(event.target.value).getTime();
+                let currentTime = new Date().getTime();
+                let dateCheck = currentTime < enteredDate;
                 this.setState({
                     dateInput: event.target.value,
+                    dateError: dateCheck,
+                    disableSubmit: !!(event.target.value && this.state.phaseInput && this.state.statusInput && this.state.noteInput && !dateCheck)
                 })
                 break;
+
             case 'phase':
                 this.setState({
                     phaseInput: event.target.value,
+                    disableSubmit: !!(this.state.dateInput && event.target.value && this.state.statusInput && this.state.noteInput && !dateCheck)
                 })
                 break;
+
             case 'status':
                 this.setState({
                     statusInput: event.target.value,
+                    disableSubmit: !!(this.state.dateInput && this.state.phaseInput && event.target.value && this.state.noteInput && !dateCheck)
                 })
                 break;
+
             case 'note':
                 this.setState({
                     noteInput: event.target.value,
+                    disableSubmit: !!(this.state.dateInput && this.state.phaseInput && this.state.statusInput && event.target.value && !dateCheck)
                 })
                 break;
+
             default:
         }
     }
@@ -45,26 +60,38 @@ class ReportForm extends Component {
         return (
             <div>
                 <div className="list">
-                    <input id="date" type="date" name="date" onChange={this.handleChange} />
-                    <select id="phase" name="phase" value="technical" onChange={this.handleChange}>
-                        <option value="technical">Technical</option>
-                        <option value="cv">CV</option>
-                        <option value="hr">HR</option>
-                        <option value="final">Final</option>
-                    </select>
-                    <select id="status" name="status" value="" onChange={this.handleChange}>
-                        <option value="">Select</option>
-                        <option value="passed">Passed</option>
-                        <option value="declined">Declined</option>
-                    </select>
+                    <p id="instruction">All fields are required!</p>
+                    <div className="input-field">
+                        <p>Interview Date:</p>
+                        <input id="date" type="date" name="date" onChange={this.handleChange} />
+                        <p id="error">{this.state.dateError ? 'Please enter valid date!' : ''}</p>
+                    </div>
+                    <div className="input-field">
+                        <p>Phase:</p>
+                        <select id="phase" name="phase" onChange={this.handleChange}>
+                            <option value="">Select</option>
+                            <option value="technical">Technical</option>
+                            <option value="cv">CV</option>
+                            <option value="hr">HR</option>
+                            <option value="final">Final</option>
+                        </select>
+                    </div>
+                    <div className="input-field">
+                        <p>Status:</p>
+                        <select id="status" name="status" onChange={this.handleChange}>
+                            <option value="">Select</option>
+                            <option value="passed">Passed</option>
+                            <option value="declined">Declined</option>
+                        </select>
+                    </div>
                     <label>
                         Notes:
-                    <textarea id="note" name="note" cols="30" rows="10" placeholder="Enter notes" value={this.state.noteInput} onChange={this.handleChange} ></textarea>
+                        <textarea id="note" name="note" cols="30" rows="10" placeholder="Enter notes" value={this.state.noteInput} onChange={this.handleChange} ></textarea>
                     </label>
                 </div>
                 <div className="buttons">
                     <button id="back" onClick={() => this.props.backButton('companies')}>Back</button>
-                    <button id="submit" onClick={() => this.props.submitButton(this.state)} >Submit</button>
+                    <button id="submit" onClick={() => this.props.submitButton(this.state)} disabled={!this.state.disableSubmit}>Submit</button>
                 </div>
             </div>
         )
--- src/app/partials/Header.js
+++ src/app/partials/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link ,withRouter} from 'react-router-dom'
 
 const Header = (props) => {
-
+    
     return (
         <nav>
             <div className="container">
                 <h1>Reports Administration</h1>
                 <div className="links">
                     <button>
-                        <Link to="/reports">Reports</Link>
+                        <Link className={props.match.path === '/reports' ? 'active-location' : ''} to="/reports">Reports</Link>
                     </button>
                     <button>
-                        <Link to="/createreport">Create Report</Link>
+                        <Link className={props.match.path === '/createreport' ? 'active-location' : ''} to="/createreport">Create Report</Link>
                     </button>
                 </div>
             </div>
@@ -21,4 +21,4 @@ const Header = (props) => {
     
 }
 
-export default Header;
+export default withRouter(Header);
